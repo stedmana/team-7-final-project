38bf1612f56f2b2c66961730d02ab135b5450a46
Yasasa Abeysirigoonawardena <yasasa.abey@gmail.com>
2018-03-14 17:12:36 -0400

    Add localization and params

---
+++ src/main/Params.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Params {
+    public static double TRACK = 0;
+    public static double WHEEL_RAD = 0;
+    public static int SPEED = 0;
+    public static int ACCEL = 0;
+}
