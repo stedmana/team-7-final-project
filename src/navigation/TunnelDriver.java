70b0ce93cdc0bb711e1f8fea15fe010ab591d093
Alex Stedman <alexander.stedman@mail.mcgill.ca>
2018-03-20 16:02:59 -0400

    removed items that would be in params

--- src/navigation/TunnelDriver.java
+++ src/navigation/TunnelDriver.java
@@ -6,47 +6,28 @@ import lejos.robotics.SampleProvider;
 import main.Params;
 import odometer.Odometer;
 import odometer.OdometerExceptions;
+import main.Params;
 
 public class TunnelDriver {
 	/**
-	 * Forward speed of the left and right motors
+	 * Forward speed of the left and right motors (deg/s)
 	 */
 	private static final int FORWARD_SPEED = 250;
-	
+
 	/**
-	 * Rotation speed for the left and right motors when turning
-	 */
-	private static final int ROTATE_SPEED = 150;
-	
-	/**
-	 * Size of tiles made by black lines
-	 */
-	private static final double TILE_SIZE = 30.48;
-	
-	/**
-	 * Amount the motor rotates to enable/disable the rear ball bearing wheel
+	 * Amount the motor rotates to enable/disable the rear ball bearing wheel (deg)
 	 */
 	private static final int ROTATION_AMOUNT = 90;
 	
 	/**
-	 * Speed of rotation of the medium motor used for rear wheel
+	 * Speed of rotation of the medium motor used for rear wheel deg/s
 	 */
 	private static final int ROTATE_SPEED_BEARING = 30;
 	
 	/**
 	 * Acceleration of medium motor
 	 */
-	private static final int ACCELERATION = 50;
-	
-	/**
-	 * Left motor on the Robot
-	 */
-	private static EV3LargeRegulatedMotor leftMotor;
-	
-	/**
-	 * Right motor on the robot
-	 */
-	private static EV3LargeRegulatedMotor rightMotor;
+	private static final int ACCELERATION = 10;
 	
 	/**
 	 * motor on the bottom of the robot, used for more precise navigation
@@ -54,34 +35,14 @@ public class TunnelDriver {
 	private static EV3MediumRegulatedMotor bearingMotor;
 	
 	/**
-	 * radius of the wheels on the robot
-	 */
-	private static double radius;
-	
-	/**
-	 * track width of the robot (distance between wheels)
-	 */
-	private static double track;
-	
-	/**
-	 * primary odometer
-	 */
-	private static Odometer odo;
-
-	/**
-	 * left light sensor
-	 */
-	private static SampleProvider leftLightVal;
-	
-	/**
-	 * right light sensor
-	 */
-	private static SampleProvider rightLightVal;
-
-	/**
-	 * ultrasonic sensor
+	 * distance recorded by the ultrasonic sensor
 	 */
 	private static SampleProvider dist;
+	
+	/**
+	 * Robots odometer
+	 */
+	private static Odometer odo;
 
 	
 	/**
@@ -95,26 +56,13 @@ public class TunnelDriver {
 	 * @param distance ultrasonic sensor data 
 	 * @throws OdometerExceptions
 	 */
-	public TunnelDriver(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, EV3MediumRegulatedMotor bearingMotor,
-			SampleProvider leftLightVal, SampleProvider rightLightVal, SampleProvider distance) throws OdometerExceptions {
+	public TunnelDriver(EV3MediumRegulatedMotor bearingMotor, SampleProvider distance) throws OdometerExceptions {
 		
-		
-		TunnelDriver.track = Params.TRACK;
-		
-		TunnelDriver.radius = Params.WHEEL_RAD;
-		
-		TunnelDriver.leftMotor = leftMotor;
-		TunnelDriver.rightMotor = rightMotor;
 		TunnelDriver.bearingMotor = bearingMotor;
-		
-		
-		
-		TunnelDriver.leftLightVal = leftLightVal;
-		TunnelDriver.rightLightVal = rightLightVal;
 		
 		TunnelDriver.dist = distance;
 		
-		odo = Odometer.getOdometer();
+		TunnelDriver.odo = Odometer.getOdometer();
 		
 	}
 	/**
